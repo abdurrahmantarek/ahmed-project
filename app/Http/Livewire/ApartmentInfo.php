8506568f89b805e0a8be886335323fb46377d3a3
abdurrahman tarek <abdurrahmantarek2@gmail.com>
2022-08-05 21:06:57 +0200

    aparment admin

---
+++ app/Http/Livewire/ApartmentInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ApartmentInfo extends Component
+{
+    public function render()
+    {
+        return view('livewire.apartment-info');
+    }
+}
