e1690c4094743f4844ba94053e0706eed233e968
Ahmed Tarik <ahmedtarik2022@gmail.com>
2022-08-05 19:08:37 +0200

    timer x2

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -14,7 +14,7 @@
     </div>
     @livewireScripts
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="{{ asset('assets/js/timer.js') }}"></script>
 </footer>
 </body>
 </html>
