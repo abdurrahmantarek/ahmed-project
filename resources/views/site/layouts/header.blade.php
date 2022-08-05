e8c924c7b2cf85bc48539cd3100a7d843e1cfdc4
Ahmed Tarik <ahmedtarik2022@gmail.com>
2022-08-05 17:05:44 +0200

    project button

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -142,7 +142,7 @@
                                 <a class="fs-15" href="{{ route('home') }}">الرئيسية</a>
                             </li>
                             <li class="active">
-                                <a class="fs-15" >المشاريع</a>
+                                <a src="#projects" class="fs-15" >المشاريع</a>
                             </li>
                             <li class="active">
                                 <a class="fs-15">الأسئلة الشائعة</a>
