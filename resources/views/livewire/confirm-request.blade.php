2a1dc747c389bb7c5d95701fd5050efea04b49d7
abdurrahman tarek <abdurrahmantarek2@gmail.com>
2023-02-26 00:15:03 +0200

    fix checkbox on sarfari;

--- resources/views/livewire/confirm-request.blade.php
+++ resources/views/livewire/confirm-request.blade.php
@@ -1,6 +1,5 @@
 <div>
-    <div class="promise" style="width: auto;
-    flex: 0 0 16px;">
+    <div class="promise custom-control custom-checkbox" style="width: auto; flex: 0 0 16px">
         <input wire:model="confirmed" type="checkbox" id="customCheck1" [(ngModel)]="checkboxValue" (change)="toggleEditable()">
         <label class="custom-control-label" for="customCheck1">أقر بصحة البيانات و المستندات المقدمه مني و اتحمل كامل المسؤليه المدنيه والجنائيه و في حالة مخالفة هذه البيانات او
             المستندات للحقيقه يعتبر الطلب لاغي ولا يعتد به دون ادني مسؤليه علي الجهة الاداريه او بنك التعميد و الاسكان</label>
