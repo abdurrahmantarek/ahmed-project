418df31fbe96359b34a3a0bc0c81d978d75b7583
mrwantarik <mrwantrik50@gmail.com>
2023-02-26 17:52:05 +0200

    fix checkbox on safari

--- resources/views/livewire/confirm-request.blade.php
+++ resources/views/livewire/confirm-request.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="promise" style="width: auto; display: flex; padding: 10px;">
-        <input wire:model="confirmed" type="checkbox" id="customCheck1" [(ngModel)]="checkboxValue" (change)="toggleEditable()">
+        <input style="width:auto;flex: 0 0 16px;" wire:model="confirmed" type="checkbox" id="customCheck1" [(ngModel)]="checkboxValue" (change)="toggleEditable()">
         <label class="custom-control-label" for="customCheck1">أقر بصحة البيانات و المستندات المقدمه مني و اتحمل كامل المسؤليه المدنيه والجنائيه و في حالة مخالفة هذه البيانات او
             المستندات للحقيقه يعتبر الطلب لاغي ولا يعتد به دون ادني مسؤليه علي الجهة الاداريه او بنك التعميد و الاسكان</label>
     </div>
